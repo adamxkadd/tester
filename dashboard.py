319304155307352857445eeb10229a47f7749fb4
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 21:12:10 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,8 +11,12 @@ st.header("Saisissez des données:")
 user_input = st.text_input("Entrez des données:", "Exemple de données")
     
 # Bouton pour envoyer les données à l'API
-if st.button("Predire"):
+if st.button("Prédire"):
     data = {"input_data": user_input}
+    st.write("Données envoyées à l'API :")
+    st.write(data)
+
+    
 #     response = requests.post(f"{API_URL}/predict/", json=data)
     
 #     if response.status_code == 200:
