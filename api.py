c0be5bfb3cbd9986b8d3a7b012845aee713556fe
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 23:39:51 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 # api.py
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class InputData(BaseModel):
+    msg: str
+
 @app.post("/hello")
-def read_root(input=):
+def read_root(input_data: InputData):
     return {"message": "Hello World"}
