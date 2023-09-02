b566c42f584b594a3fe351a018a9641eaa0c6509
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 23:39:24 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,13 +2,16 @@ import streamlit as st
 import requests
 import json
 
-# st.title("Hello World with FastAPI and Streamlit")
-# Faites une requête à votre API FastAPI
-# response = requests.get("https://tester-api.streamlit.app/hello/")
-response = requests.post(url = "https://tester-api.streamlit.app/hello", data = json.dumps({"msg":'c moi'}))
-response.text
-# response.raise_for_status()  # Vérifiez si la réponse est valide
-# data = response.json()
-# st.write("Réponse de l'API FastAPI :")
-# st.write(data)
+# Utilisez le bon endpoint URL
+url = "https://tester-api.streamlit.app/hello/"
 
+# Utilisez le bon format de données pour la requête
+data = json.dumps({"msg": "c moi"})
+
+# Spécifiez le type de contenu JSON dans l'en-tête
+headers = {'Content-Type': 'application/json'}
+
+response = requests.post(url=url, data=data, headers=headers)
+
+# Affichez la réponse
+st.write(response.text)
