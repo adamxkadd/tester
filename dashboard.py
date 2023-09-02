e0aa2beac04d64f09dd088f1c7d76fdfbaef9be9
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 00:03:16 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,16 +2,11 @@ import streamlit as st
 import requests
 import json
 
-# Utilisez le bon endpoint URL
 url = "https://tester-api.streamlit.app/hello"
-
-# Utilisez le bon format de données pour la requête
 data = json.dumps({"msg": "c moi"})
-
-# Spécifiez le type de contenu JSON dans l'en-tête
 headers = {'Content-Type': 'application/json'}
-
 response = requests.post(url=url, data=data, headers=headers)
-
-# Affichez la réponse
+st.write("affiche data")
+st.write(data.text)
+st.write("affiche reponse")
 st.write(response.text)
