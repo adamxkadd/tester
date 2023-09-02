52d5df3518157aaec1197552e91d06e355dc68cf
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 21:52:23 +0200

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.post('predict/')
+@app.post('/predict/')
 async def predict(data: dict):
     # Simulez une prédiction ici
     print("La fonction predict a débuté son exécution.")
