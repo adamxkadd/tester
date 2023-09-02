5456c85e16a17e2982d4a96bbd86221681e45c79
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 23:35:54 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,17 +2,13 @@ import streamlit as st
 import requests
 import json
 
-st.title("Hello World with FastAPI and Streamlit")
+# st.title("Hello World with FastAPI and Streamlit")
+# Faites une requête à votre API FastAPI
+# response = requests.get("https://tester-api.streamlit.app/hello/")
+response = requests.post(url = "https://tester-api.streamlit.app/hello/", data = json.dumps({"msg":'c moi'}))
+response.text
+# response.raise_for_status()  # Vérifiez si la réponse est valide
+# data = response.json()
+# st.write("Réponse de l'API FastAPI :")
+# st.write(data)
 
-
-
-try:
-    # Faites une requête à votre API FastAPI
-    # response = requests.get("https://tester-api.streamlit.app/hello/")
-    response = requests.post(url = "https://tester-api.streamlit.app/hello/", data = json.dumps())
-    response.raise_for_status()  # Vérifiez si la réponse est valide
-    data = response.json()
-    st.write("Réponse de l'API FastAPI :")
-    st.write(data)
-except requests.exceptions.RequestException as e:
-    st.error(f"Une erreur s'est produite : {e}")
