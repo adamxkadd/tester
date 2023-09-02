43d025ad59f2b41bb8017cbe2cb014277d6d7dde
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 23:36:20 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import json
 # st.title("Hello World with FastAPI and Streamlit")
 # Faites une requête à votre API FastAPI
 # response = requests.get("https://tester-api.streamlit.app/hello/")
-response = requests.post(url = "https://tester-api.streamlit.app/hello/", data = json.dumps({"msg":'c moi'}))
+response = requests.post(url = "https://tester-api.streamlit.app/hello", data = json.dumps({"msg":'c moi'}))
 response.text
 # response.raise_for_status()  # Vérifiez si la réponse est valide
 # data = response.json()
