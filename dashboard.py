849114f98c75c4fe850c765c54357d9bb08260cf
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 21:05:14 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,25 +2,16 @@
 import streamlit as st
 import requests
 
-API_URL = "https://scoe-banque.streamlit.app/"  # Mettez l'URL de votre API FastAPI ici
+API_URL = "https://scoe-banque.streamlit.app"  # Mettez l'URL de votre API FastAPI ici
 
 st.title("Tableau de bord Streamlit")
 
 # Créez un formulaire pour saisir des données
 st.header("Saisissez des données:")
 user_input = st.text_input("Entrez des données:", "Exemple de données")
-
-data = {"input_data": user_input}
-response = requests.post(f"{API_URL}/predict/", json=data)
-
-if response.status_code == 200:
-    result = response.json()
-    st.success(result["result"])
-else:
-    st.error("Une erreur s'est produite lors de la prédiction.")
-        
+    
 # Bouton pour envoyer les données à l'API
-if st.button("Envoyer"):
+if st.button("Predire"):
     data = {"input_data": user_input}
     response = requests.post(f"{API_URL}/predict/", json=data)
     
