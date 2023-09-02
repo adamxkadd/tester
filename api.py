d734687e7d605066d48d8f643a25d49a243b2645
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 22:28:00 +0200

    Update api.py

--- api.py
+++ api.py
@@ -5,11 +5,7 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-
 @app.post('/predict/')
 async def predict(data: dict):
-    # Simulez une prédiction ici
-    prediction_result = {"result": "Prédiction effectuée avec succès", "data": data}
-    
-    # Utilisez JSONResponse pour renvoyer une réponse JSON personnalisée
-    return JSONResponse(content=prediction_result)
+    response_data = {"result": "Prédiction effectuée avec succès", "data": data}
+    return JSONResponse(content=response_data)
