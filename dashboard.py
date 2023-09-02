541f5a6d0c3b177a20646e17437b8ebdf9227bf0
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 00:09:10 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,8 +6,16 @@ url = "https://tester-api.streamlit.app/hello"
 data = json.dumps({"msg": "c moi"})
 headers = {'Content-Type': 'application/json'}
 response = requests.post(url=url, data=data, headers=headers)
-st.write("affiche data")
+
+# Affiche les données envoyées dans la requête
+st.write("Contenu de data:")
 st.write(data)
-st.write(data.msg)
-st.write("affiche reponse")
-st.write(response.text)
+
+# Vérifie si la requête a réussi
+if response.status_code == 200:
+    st.write("Réponse de l'API:")
+   # st.write(response.text)
+    st.write(response.json())
+else:
+    st.write("La requête a échoué avec le code de statut:", response.status_code)
+
