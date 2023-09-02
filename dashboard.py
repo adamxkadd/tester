7eec7473d9290d3c04c2a67af0a44166b3a3ff58
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-02 22:05:07 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,16 +9,35 @@ st.title("Tableau de bord Streamlit")
 # Créez un formulaire pour saisir des données
 st.header("Saisissez des données:")
 user_input = st.text_input("Entrez des données:", "Exemple de données")
-    
-# Bouton pour envoyer les données à l'API
+
 if st.button("Prédire"):
     data = {"input_data": user_input} 
     response = requests.post(f"{API_URL}/predict/", json=data)
-    st.write("reponse : ") 
+    st.write("Réponse : ") 
     st.write(response)
-    st.write(response.status_code)
-    st.write("réponse JSON :")
-    st.write(response.json())
+    st.write("Code de réponse HTTP : ", response.status_code)
+    
+    # Vérifiez si la réponse a un code de statut 200 (OK)
+    if response.status_code == 200:
+        try:
+            result = response.json()
+            st.write("Contenu de la réponse JSON :")
+            st.write(result)
+        except json.decoder.JSONDecodeError as e:
+            st.error("Erreur lors de l'analyse de la réponse JSON.")
+    else:
+        st.error("Une erreur s'est produite lors de la prédiction. Code de statut HTTP : ", response.status_code)
+
+
+# # Bouton pour envoyer les données à l'API
+# if st.button("Prédire"):
+#     data = {"input_data": user_input} 
+#     response = requests.post(f"{API_URL}/predict/", json=data)
+#     st.write("reponse : ") 
+#     st.write(response)
+#     st.write(response.status_code)
+#     st.write("réponse JSON :")
+#     st.write(response.json())
 
 #     if response.status_code == 200:
 #         result = response.json()
