00e5d8e97efe347c118f79fc9182583422c08bd7
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 00:05:52 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,5 +8,6 @@ headers = {'Content-Type': 'application/json'}
 response = requests.post(url=url, data=data, headers=headers)
 st.write("affiche data")
 st.write(data)
+st.write(data.msg)
 st.write("affiche reponse")
 st.write(response.text)
